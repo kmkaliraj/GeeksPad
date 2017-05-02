a14b4de66b0b6bf8e3e3b1af07eaecf4d5def46d
Sreerag <Sreerag>
2017-05-02 01:42:48 -0700

    profile added

--- app/src/main/java/com/example/sreer/geekspad/ui/activity/HomePageActivity.java
+++ app/src/main/java/com/example/sreer/geekspad/ui/activity/HomePageActivity.java
@@ -1,5 +1,8 @@
 package com.example.sreer.geekspad.ui.activity;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
@@ -8,26 +11,41 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
-
 import com.example.sreer.geekspad.R;
+import com.example.sreer.geekspad.ui.fragment.ProfileViewFragment;
 
 public class HomePageActivity extends AppCompatActivity {
 
     private BottomNavigationView mBottomNav;
+    private ProfileViewFragment userProfile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-
         mBottomNav = (BottomNavigationView) findViewById(R.id.navigation);
         mBottomNav.getMenu().getItem(1).setChecked(true);
         mBottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected( MenuItem item) {
-              //  handleMenuSelection(item);
+               handleMenuSelection(item);
                 return true;
             }
         });
+    }
+
+    public void handleMenuSelection(MenuItem item){
+        FragmentManager manager = getFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.detailFragment);
+        FragmentTransaction fragmentTransaction = manager.beginTransaction();
+        switch (item.getItemId()) {
+            case R.id.menu_home:
+                    ProfileViewFragment userProfile = new ProfileViewFragment();
+                    fragmentTransaction.replace(R.id.detailFragment, userProfile);
+                    break;
+        }
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 
     @Override
@@ -45,7 +63,6 @@ public class HomePageActivity extends AppCompatActivity {
                 logout();
                 break;
         }
-
         return true;
     }
 
